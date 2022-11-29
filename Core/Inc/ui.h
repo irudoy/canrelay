e1235b086cd6c01a5e971ed29bfb49b3773ee7c4
Ivan Rudoy <rudoy@farpost.com>
2022-11-30 06:27:12 +1000

    add notes

--- Core/Inc/ui.h
+++ Core/Inc/ui.h
@@ -6,6 +6,8 @@
 #define UI_COLOR_BLUE 0x0a6eff
 #define UI_COLOR_GRAY 0x6a6b70
 
+// https://fontawesome.com/v6/icons/?o=r&m=free&s=solid
+// https://onlinestringtools.com/convert-string-to-ascii Hexademical ASCII
 #define LV_CUSTOM_SYMBOL_PLUG "\xEF\x87\xA6" /*61926, 0xF1E6*/
 #define LV_CUSTOM_SYMBOL_BUG "\xEF\x86\x88" /*61832, 0xF188*/
 #define LV_CUSTOM_SYMBOL_BRIGHTNESS "\xEF\x86\x85" /*61829, 0xF185*/
